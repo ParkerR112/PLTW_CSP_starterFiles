e8f21f38b9268ed0c29c99ced3092be32b1a548e
ParkerR112 <parker.rodgers@nevadacubs.org>
2022-09-13 13:08:36 -0500

    1.1.6 edits 2.0

--- 1.1.6/a116_buggy_image.py
+++ 1.1.6/a116_buggy_image.py
@@ -5,23 +5,38 @@ import turtle as trtl
 
 # draw the body of the spider
 spider = trtl.Turtle()
+spider.shape("circle")
 spider.pensize(40)
 spider.circle(20)
+
 
 # configure spider legs
 numOfLegs = 8
 legLength = 70
-degreeOfTurn = 360 / numOfLegs
+degreeOfTurn = 270 / numOfLegs
+spider.color("black")
 spider.pensize(5)
 interator = 0
 
 # draw the legs of the spider
 while (interator < numOfLegs):
   spider.goto(0,20)
-  spider.setheading(degreeOfTurn*interator)
+  spider.setheading((degreeOfTurn*interator) - 27)
   spider.forward(legLength)
   interator += 1
-  
+
+# draw the eyes of the spider
+spider.penup()
+spider.pensize(5)
+spider.goto(-15, 10)
+spider.pendown()
+spider.color("white")
+spider.stamp()
+spider.penup()
+spider.goto(15, 10)
+spider.pendown()
+spider.stamp()
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
