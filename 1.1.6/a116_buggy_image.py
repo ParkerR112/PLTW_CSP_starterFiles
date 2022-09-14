65ce381830eac655f4417be2e56ee4b10a37fa4c
ParkerR112 <parker.rodgers@nevadacubs.org>
2022-09-14 13:05:31 -0500

    1.1.6_9-14-22

--- 1.1.6/a116_buggy_image.py
+++ 1.1.6/a116_buggy_image.py
@@ -2,6 +2,10 @@
 import turtle as trtl
 # instead of a descriptive name of the turtle such as painter,
 # a less useful variable name x is used
+def move(x, y):
+  spider.penup()
+  spider.goto(x, y)
+  spider.pendown()
 
 # draw the body of the spider
 spider = trtl.Turtle()
@@ -9,6 +13,11 @@ spider.shape("circle")
 spider.pensize(40)
 spider.circle(20)
 
+# draw the head of the spider
+move(0, -45)
+spider.shape("circle")
+spider.pensize(30)
+spider.circle(10)
 
 # configure spider legs
 numOfLegs = 8
@@ -20,21 +29,17 @@ interator = 0
 
 # draw the legs of the spider
 while (interator < numOfLegs):
-  spider.goto(0,20)
+  move(0,20)
   spider.setheading((degreeOfTurn*interator) - 27)
   spider.forward(legLength)
   interator += 1
 
 # draw the eyes of the spider
-spider.penup()
-spider.pensize(5)
-spider.goto(-15, 10)
-spider.pendown()
-spider.color("white")
+spider.shapesize(1)
+move(-10, -40)
+spider.color("purple")
 spider.stamp()
-spider.penup()
-spider.goto(15, 10)
-spider.pendown()
+move(10, -40)
 spider.stamp()
 
 spider.hideturtle()
