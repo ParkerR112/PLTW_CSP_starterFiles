3bc4dd17233e4a586ec7f0833c896bcabe866bcb
ParkerR112 <parker.rodgers@nevadacubs.org>
2022-09-20 12:43:05 -0500

    1.1.7

--- 1.1.7/a117_traversing_turtles.py
+++ 1.1.7/a117_traversing_turtles.py
@@ -6,26 +6,32 @@ import turtle as trtl
 my_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  t.penup()
   my_turtles.append(t)
 
-#  
+#  set the starting x and y
 startx = 0
 starty = 0
-
-#
+startheading = 0
+# create the loop to draw the shape
 for t in my_turtles:
+  index = my_turtles.index(t)
+  t.color(turtle_colors[index])
   t.goto(startx, starty)
-  t.right(45)     
-  t.forward(50)
+  t.setheading(startheading)
+  t.pendown()
+  t.right(70)     
+  t.forward(70)
 
-#	
-  startx = startx + 50
-  starty = starty + 50
+#	increment the startx and starty variables
+  startx = t.xcor()
+  starty = t.ycor()
+  startheading = t.heading()
 
 wn = trtl.Screen()
 wn.mainloop()
