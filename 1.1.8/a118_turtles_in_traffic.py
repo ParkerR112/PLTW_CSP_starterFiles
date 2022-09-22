fce7d5f9ac2f069ea6a7bb5ff029a2cb16f66e00
ParkerR112 <parker.rodgers@nevadacubs.org>
2022-09-22 12:50:36 -0500

    1.1.8

--- 1.1.8/a118_turtles_in_traffic.py
+++ 1.1.8/a118_turtles_in_traffic.py
@@ -8,9 +8,11 @@ horiz_turtles = []
 vert_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
-vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+turtle_shapes = ["classic", "turtle", "circle", "square", "triangle"]
+collision_shape = "arrow"
+collision_color = "brown"
+horiz_colors = ["red", "blue", "green", "orange", "purple"]
+vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo"]
 
 tloc = 50
 for s in turtle_shapes:
@@ -34,11 +36,21 @@ for s in turtle_shapes:
   tloc += 50
 
 # TODO: move turtles across and down screen, stopping for collisions
-"""
-steps = 0
-while steps < 50:
-	steps = steps + 1
-"""
+
+for step in range(50):
+  turtles = vert_turtles + horiz_turtles
+  for t in turtles:
+    t.forward(10)
+
+  for vt in vert_turtles:
+    for ht in horiz_turtles:
+      if abs(ht.xcor() - vt.xcor()) <= 10 and abs(ht.ycor() - vt.ycor()) <= 10:
+        vt.shape(collision_shape)
+        vt.color(collision_color)
+        ht.shape(collision_shape)
+        ht.color(collision_color)
+        vert_turtles.remove(vt)
+        horiz_turtles.remove(ht)
 
 wn = trtl.Screen()
 wn.mainloop()
